87c1e4154d68083719d46c4cb078f4c63df9165e
Vishesh S Rajput <131168255+Asamaurdhava@users.noreply.github.com>
2024-10-24 22:28:02 +0530

    Add files via upload

---
+++ practicingFor.py
@@ -0,0 +1,5 @@
+print("The program showcases the working of nested for loops.\n")
+
+for i in range(1,11):
+    for j in range(11,21):
+        print(i * j)    #prints the multiplication of 1 to 10 with 11 to 20 respectively
